6df09358ae5573e952ea385de99db7132e096256
Pukimaa <pukima@pukima.site>
2023-10-08 22:48:33 +0200

    add de + round images + add user context menu docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,56 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tako-docs-astro.netlify.app/",
-  integrations: [
-    starlight({
-      favicon: "favicon.png",
-      title: "Tako User Docs",
-      logo: {
-        light: "./src/assets/wordmark_light.svg",
-        dark: "./src/assets/wordmark_dark.svg",
-        replacesTitle: true,
-      },
-      social: {
-        github: "https://github.com/tako-discord/docs-astro",
-        discord: "https://discord.gg/wDm8Qe6mud",
-      },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Getting Started", link: "/guides/getting-started/" },
-          ],
-        },
-        {
-          label: "Info",
-          autogenerate: { directory: "info" },
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-      customCss: ["./src/tailwind.css"],
-      components: {
-        SiteTitle: "./src/components/SiteTitle.astro",
-      },
-    }),
-    tailwind({ applyBaseStyles: false }),
-  ],
+	site: 'https://tako-docs-astro.netlify.app/',
+	integrations: [
+		starlight({
+			favicon: 'favicon.png',
+			title: 'Tako User Docs',
+			logo: {
+				light: './src/assets/wordmark_light.svg',
+				dark: './src/assets/wordmark_dark.svg',
+				replacesTitle: true,
+			},
+			social: {
+				github: 'https://github.com/tako-discord/docs-astro',
+				discord: 'https://discord.gg/wDm8Qe6mud',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Getting Started', link: '/guides/getting-started/' },
+					],
+				},
+				{
+					label: 'Info',
+					autogenerate: { directory: 'info' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: 'English',
+					lang: 'en',
+				},
+				de: {
+					label: 'Deutsch',
+					lang: 'de',
+				},
+			},
+			customCss: ['./src/tailwind.css'],
+			components: {
+				SiteTitle: './src/components/SiteTitle.astro',
+			},
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
 });
---
+++ src/content/docs/de/guides/getting-started.md
@@ -0,0 +1,9 @@
+---
+title: Lege los
+description: Ein Guide um mit Tako anzufangen
+---
+:::note
+Du liest gerade die Dokumentation für die **nächste** version von Tako. Deshalb könnte es sein, dass diese Dokumentation nicht vollständig, oder falsch ist.
+:::
+
+Komm bald™️ wieder, für mehr Informationen!
