d2b7f78da94190d4df477b68189370ff31a9c998
Pukimaa <pukima@pukima.site>
2023-12-06 14:27:11 +0000

    fix(astro.config.mjs): fix missing import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 
